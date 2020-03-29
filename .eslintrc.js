2746c6f68a5f39b1c8ed7a85c0112fc05a094295
cntchen <cntchen@gmail.com>
2020-03-29 18:14:50 +0800

    build: update eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,10 @@ module.exports = {
         "@typescript-eslint/eslint-plugin"
     ],
     rules: {
-
+        "@typescript-eslint/ban-ts-ignore": 0,
+        "@typescript-eslint/interface-name-prefix": 0,
+        "@typescript-eslint/no-explicit-any": 0,
+        "@typescript-eslint/no-empty-function": 0,
+        "@typescript-eslint/no-namespace": 0
     },
 };
