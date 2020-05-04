9becae4000c415881c67d1794d19806229eac7fd
cntchen <cntchen@gmail.com>
2020-05-04 14:09:45 +0800

    feat: export main thread worker to global for debug

--- src/page/index.ts
+++ src/page/index.ts
@@ -1,10 +1,12 @@
 import createAlloyWorker from '../worker/index';
 
-// console.log('test', __WORKER__);
-
 // 初始化 AlloyWorker
 const alloyWorker = createAlloyWorker({
     workerName: 'alloyWorker--test',
 });
+
+// 挂载到全局环境, 用于调试
+// @ts-ignore
+window.alloyWorker = alloyWorker;
 
 console.log('alloyWorker', alloyWorker);
