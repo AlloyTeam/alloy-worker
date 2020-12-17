d9cdebb93c49f0a4bc7dfed1c7be9ca75a15e814
cntchen <cntchen@tencent.com>
2020-12-17 20:18:58 +0800

    build: refactor e2e test config

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,8 +1,9 @@
 // jest-puppeteer.config.js
 module.exports = {
     server: {
-      command: 'npm run dist && http-server -p 6842 -c0 ./dist',
-      port: 6842,
-      launchTimeout: 20000,
+        // 启动生产模式构建, 并通过 http-server 启动本地访问
+        command: 'npm run dist && http-server -p 6842 -c0 ./dist',
+        port: 6842,
+        launchTimeout: 20000,
     },
-  }
+}
